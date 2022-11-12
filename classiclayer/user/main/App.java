5f3c7d870ef5c25f5ea6ef3af1623deba6f57c0f
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2022-11-12 08:57:45 -0300

    adding main package

---
+++ classiclayer/user/main/App.java
@@ -0,0 +1,14 @@
+package user.main;
+
+public class App {
+
+  public static void main(String[] args) {
+    // This class inject everything !
+    // and bootstrap the app
+
+    // if you use Spring is your SpringApplication
+
+  }
+
+}
+
---
+++ orm/user/main/Main.java
@@ -0,0 +1,13 @@
+package user.main;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // This class inject everything !
+    // and bootstrap the app
+
+    // if you use Spring is your SpringApplication
+
+  }
+
+}
